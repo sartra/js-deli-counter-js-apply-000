8653fba4f0e3ede69561a9621ea302d11f1c6002
SARTRA <strenshaw@gmail.com>
2017-06-05 08:28:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function  takeANumber(katzDeliLine, name){
   return katzDeliLine.indexOf(name) + 1; 
 }
+
+function nowServing(katzDeliLine){
+  var person = katzDeliLine.shift();
+  console.log(person, katzDeliLine);
+  if (!katzDeliLine.length){
+    return "There is nobody waiting to be served!";
+  }
+}
